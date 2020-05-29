df5ef7e1affe7425afc2dce3e64c03c556b82665
Ryan <ryanhornby1999@gmail.com>
2020-05-29 13:40:48 -0700

    changed data format

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' CEdata
+#'
+#' Sample of consumer expenditure survey
+#'
+#' @format A data frame with 5571 rows and 7 variables:
+#' \describe{
+#'   \item{Age}{Age of reference person}
+#'   \item{Urban}{Is this conumer unit located in an urban or rural area}
+#'   \item{Tenrue}{Housing tenure}
+#'   \item{Educ}{Education of reference person}
+#'   \item{Expenditure}{Total expenditure last quarter}
+#'   \item{Marital}{Marital status of reference person}
+#'   \item{Income}{Total amount of family income before taxes in the last 12 months}
+#' }
+"CEdata"
